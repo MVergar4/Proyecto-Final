784c849f70a274c96afc9e48e3ff2fabb072c248
MVergar4 <marcvergara2023@udec.cl>
2024-06-21 02:22:31 -0400

    Añadida primera version de como se vería un autobus para seleccionar en la pantalla

---
+++ Proyecto-Final/src/main/java/org/Grafica/PantallaConcepcion.java
@@ -0,0 +1,11 @@
+package org.Grafica;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PantallaConcepcion extends JPanel {
+    public PantallaConcepcion() {
+        super();
+        setBackground(Color.cyan);
+    }
+}
