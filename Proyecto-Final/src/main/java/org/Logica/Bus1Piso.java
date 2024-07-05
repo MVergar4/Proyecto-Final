05f8b518f8bb7130373b3bed06beda890d475dc6
MVergar4 <marcvergara2023@udec.cl>
2024-07-05 00:49:04 -0400

    Añadido nuevo metodo reservarAsiento

--- Proyecto-Final/src/main/java/org/Logica/Bus1Piso.java
+++ Proyecto-Final/src/main/java/org/Logica/Bus1Piso.java
@@ -3,13 +3,17 @@ package org.Logica;
 public class Bus1Piso extends Bus {
     public Bus1Piso() {
         for (int i = 0; i < 4; i++) {
-            super.asientos.add(new AsientoSuiteCama("0" + i));
+            asientos.add(new AsientoSuiteCama("0" + i));
         }
         for (int i = 0; i < 12; i++) {
-            super.asientos.add(new AsientoSalonCama("1" + i));
+            asientos.add(new AsientoSalonCama("1" + i));
         }
         for (int i = 0; i < 16; i++) {
-            super.asientos.add(new AsientoSemiCama("2" + i));
+            asientos.add(new AsientoSemiCama("2" + i));
         }
+    }
+
+    @Override
+    public void reservarAsiento(String letra, int numero) {
     }
 }
