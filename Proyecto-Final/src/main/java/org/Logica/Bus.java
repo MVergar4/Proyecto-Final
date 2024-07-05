05d36ae431a772ffcfb76ba0f83c08cd797ff6a8
MVergar4 <marcvergara2023@udec.cl>
2024-07-05 00:49:04 -0400

    Añadido nuevo metodo reservarAsiento

--- Proyecto-Final/src/main/java/org/Logica/Bus.java
+++ Proyecto-Final/src/main/java/org/Logica/Bus.java
@@ -4,4 +4,5 @@ import java.util.ArrayList;
 
 public abstract class Bus {
     protected ArrayList<Asiento> asientos = new ArrayList<>();
+    public abstract void reservarAsiento(String letra, int numero);
 }
--- Proyecto-Final/src/main/java/org/Logica/Bus2Piso.java
+++ Proyecto-Final/src/main/java/org/Logica/Bus2Piso.java
@@ -3,13 +3,18 @@ package org.Logica;
 public class Bus2Piso extends Bus {
     public Bus2Piso() {
         for (int i = 0; i < 8; i++) {
-            super.asientos.add(new AsientoSuiteCama("0" + i));
+            super.asientos.add(new AsientoSuiteCama("3" + i));
         }
         for (int i = 0; i < 24; i++) {
-            super.asientos.add(new AsientoSalonCama("1" + i));
+            super.asientos.add(new AsientoSalonCama("4" + i));
         }
         for (int i = 0; i < 32; i++) {
-            super.asientos.add(new AsientoSemiCama("2" + i));
+            super.asientos.add(new AsientoSemiCama("5" + i));
         }
+    }
+
+    @Override
+    public void reservarAsiento(String letra, int numero) {
+
     }
 }
