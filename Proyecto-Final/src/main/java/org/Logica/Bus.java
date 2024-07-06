08ea68093d959ea9d57a23b4d663a1eb5d058e82
pcfuenr <panchocifuentes1234@gmail.com>
2024-07-06 13:43:56 -0400

    Arreglado switch case de la clase bus

--- Proyecto-Final/src/main/java/org/Logica/Bus.java
+++ Proyecto-Final/src/main/java/org/Logica/Bus.java
@@ -29,14 +29,11 @@ public abstract class Bus {
     private int transformarFila(String letra, int numero) {
         int contador = -100;
         switch (letra) {
-            case "A":
-                contador = 0;
-            case "B":
-                contador = 1;
-            case "C":
-                contador = 2;
-            case "D":
-                contador = 3;
+            case "A": contador = 0; break;
+            case "B": contador = 1; break;
+            case "C": contador = 2; break;
+            case "D": contador = 3; break;
+            default: break;
         }
         return contador + ((numero - 1) * 4);
     }
