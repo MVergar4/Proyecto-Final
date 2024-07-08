7c223ceb6d48bb67a58a7316c23f1e3dbd463603
MVergar4 <marcvergara2023@udec.cl>
2024-07-08 05:50:30 -0400

    Añadidos asserts al test de reservar asientos

--- Proyecto-Final/src/test/java/org/example/ReservarAsientoTest.java
+++ Proyecto-Final/src/test/java/org/example/ReservarAsientoTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.Logica.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ReservarAsientoTest {
     private final Director director = new Director();
@@ -23,21 +24,21 @@ class ReservarAsientoTest {
             bus1.reservarAsiento("A",3);
             bus1.reservarAsiento("B", 6);
             bus1.reservarAsiento("D", 1);
-            bus1.reservarAsiento("C", 7);
-            bus1.reservarAsiento("A", 5);
-            bus1.reservarAsiento("C", 8);
             bus1.reservarAsiento("C", 2);
 
             bus2.reservarAsiento("C", 14);
             bus2.reservarAsiento("A", 1);
-            bus2.reservarAsiento("B", 8);
-            bus2.reservarAsiento("B", 4);
             bus2.reservarAsiento("D", 7);
-            bus2.reservarAsiento("A", 11);
             bus2.reservarAsiento("C", 16);
         } catch (AsientoOcupadoException e) {
             System.out.println(e.getMessage());
         }
+        assertFalse(bus1.revisarAsiento("A", 3));
+        assertFalse(bus1.revisarAsiento("D", 1));
+        assertFalse(bus1.revisarAsiento("C", 2));
+        assertFalse(bus2.revisarAsiento("A", 1));
+        assertFalse(bus2.revisarAsiento("C", 16));
+        assertFalse(bus2.revisarAsiento("D", 7));
     }
 
     @Test
@@ -47,9 +48,10 @@ class ReservarAsientoTest {
             bus1.reservarAsiento("B", 5);
             bus1.reservarAsiento("A", 6);
             bus1.reservarAsiento("C", 7);
-
         } catch (AsientoOcupadoException e) {
             System.out.println(e.getMessage());
         }
+        assertFalse(bus1.revisarAsiento("A", 6));
+        assertFalse(bus1.revisarAsiento("B", 5));
     }
 }
