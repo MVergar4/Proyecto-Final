66ac7db4e117a9ba171f46170a418e2d8b66d82f
MVergar4 <marcvergara2023@udec.cl>
2024-07-08 00:07:54 -0400

    Creados todos los metodos de Director para construir Buses de distinto tipo

--- Proyecto-Final/src/main/java/org/Logica/Director.java
+++ Proyecto-Final/src/main/java/org/Logica/Director.java
@@ -4,14 +4,64 @@ import java.time.LocalTime;
 
 public class Director {
     public Director() {}
-    public void Bus1PisoMatutinoSimple(Builder b) {
+    public void Bus1PisoSimple(Builder b, String horario) {
         b.reset();
         b.setAsientos(20, 12, 0);
-        b.setHorario(LocalTime.of(6,0));
+        switch (horario) {
+            case "Matutino": b.setHorario(LocalTime.of(6, 0)); break;
+            case "Mediodia": b.setHorario(LocalTime.of(12, 0)); break;
+            case "Tarde": b.setHorario(LocalTime.of(18, 0)); break;
+            case "Nocturno": b.setHorario(LocalTime.of(0, 0)); break;
+        }
     }
-    public void Bus2PisosMatutinoSimple(Builder b) {
+    public void Bus2PisosSimple(Builder b, String horario) {
         b.reset();
         b.setAsientos(40, 24, 0);
-        b.setHorario(LocalTime.of(6, 0));
+        switch (horario) {
+            case "Matutino": b.setHorario(LocalTime.of(6, 0)); break;
+            case "Mediodia": b.setHorario(LocalTime.of(12, 0)); break;
+            case "Tarde": b.setHorario(LocalTime.of(18, 0)); break;
+            case "Nocturno": b.setHorario(LocalTime.of(0, 0)); break;
+        }
+    }
+    public void Bus1PisoMedio(Builder b, String horario) {
+        b.reset();
+        b.setAsientos(16, 12, 4);
+        switch (horario) {
+            case "Matutino": b.setHorario(LocalTime.of(6, 0)); break;
+            case "Mediodia": b.setHorario(LocalTime.of(12, 0)); break;
+            case "Tarde": b.setHorario(LocalTime.of(18, 0)); break;
+            case "Nocturno": b.setHorario(LocalTime.of(0, 0)); break;
+        }
+    }
+    public void Bus2PisosMedio(Builder b, String horario) {
+        b.reset();
+        b.setAsientos(32, 20, 12);
+        switch (horario) {
+            case "Matutino": b.setHorario(LocalTime.of(6, 0)); break;
+            case "Mediodia": b.setHorario(LocalTime.of(12, 0)); break;
+            case "Tarde": b.setHorario(LocalTime.of(18, 0)); break;
+            case "Nocturno": b.setHorario(LocalTime.of(0, 0)); break;
+        }
+    }
+    public void Bus1PisoDeluxe(Builder b, String horario) {
+        b.reset();
+        b.setAsientos(0, 16, 16);
+        switch (horario) {
+            case "Matutino": b.setHorario(LocalTime.of(6, 0)); break;
+            case "Mediodia": b.setHorario(LocalTime.of(12, 0)); break;
+            case "Tarde": b.setHorario(LocalTime.of(18, 0)); break;
+            case "Nocturno": b.setHorario(LocalTime.of(0, 0)); break;
+        }
+    }
+    public void Bus2PisosDeluxe(Builder b, String horario) {
+        b.reset();
+        b.setAsientos(0, 32, 32);
+        switch (horario) {
+            case "Matutino": b.setHorario(LocalTime.of(6, 0)); break;
+            case "Mediodia": b.setHorario(LocalTime.of(12, 0)); break;
+            case "Tarde": b.setHorario(LocalTime.of(18, 0)); break;
+            case "Nocturno": b.setHorario(LocalTime.of(0, 0)); break;
+        }
     }
 }
